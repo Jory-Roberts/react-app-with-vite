f2b7f38c869078977bb315a2011df03d51837353
Jory Roberts <robertsjory13@gmail.com>
2023-05-23 21:36:32 -0500

    changes form and styles

--- react-practice/src/Form.jsx
+++ react-practice/src/Form.jsx
@@ -1,11 +1,14 @@
 const Form = () => {
     return (
         <form className="form">
-            <label htmlFor="name">Name</label>
-            <input autoFocus id="addName" placeholder="Add Name" required />
-
-            <label htmlFor="email">Email</label>
-            <input autoFocus id="addEmail" placeholder="Add Email" required />
+            <label htmlFor="item">Item</label>
+            <input
+                type="text"
+                autoFocus
+                id="addItem"
+                placeholder="Add Item"
+                required
+            />
 
             <button type="submit" aria-label="Add Information"></button>
         </form>
