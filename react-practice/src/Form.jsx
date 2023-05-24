dfeed2c4d2b8be94a1ab7aaefa65274cf5e3a233
Jory Roberts <robertsjory13@gmail.com>
2023-05-24 00:29:48 -0500

    adds form validation

--- react-practice/src/Form.jsx
+++ react-practice/src/Form.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 const Form = () => {
+    const [item, setItem] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setItem('');
+    };
     return (
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
             <label htmlFor="item">Item</label>
             <input
                 type="text"
                 autoFocus
                 id="addItem"
                 placeholder="Add Item"
+                value={item}
+                onChange={(e) => setItem(e.target.value)}
                 required
             />
 
